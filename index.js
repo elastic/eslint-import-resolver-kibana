3eea7984d6ac12e6953f6e5e0c46b56c035c054b
spalger <spalger@users.noreply.github.com>
2017-05-31 16:31:54 -0700

    [resolver] add default aliases from test_bundle

--- index.js
+++ index.js
@@ -50,11 +50,18 @@ function getPlugins(config, projectRoot) {
 function getWebpackConfig(source, projectRoot, config) {
   const kibanaPath = getKibanaPath(config, projectRoot);
   const fromKibana = (...path) => resolve(kibanaPath, ...path);
-    
+  
   const aliases = {
+    // Kibana defaults https://github.com/elastic/kibana/blob/b7f519704ae0d25f085e3278198e2abec1d9ef6e/src/ui/ui_bundler_env.js#L30-L34
     ui: fromKibana('src/ui/public'),
     test_harness: fromKibana('src/test_harness/public'),
     querystring: 'querystring-browser',
+    
+    // Dev defaults for test bundle https://github.com/elastic/kibana/blob/b7f519704ae0d25f085e3278198e2abec1d9ef6e/src/core_plugins/tests_bundle/index.js#L70-L75
+    ng_mock$: fromKibana('src/core_plugins/dev_mode/public/ng_mock'),
+    'angular-mocks$': fromKibana('src/core_plugins/tests_bundle/webpackShims/angular-mocks.js'),
+    fixtures: fromKibana('src/fixtures'),
+    test_utils: fromKibana('src/test_utils/public'),
   };
   
   getPlugins(config, projectRoot).forEach(plugin => {
